84e0cf58337a82c9c7db643673dbeab407c79109
Hunter <haufhun@gmail.com>
2018-03-07 00:11:20 -0600

    Create open.sh script for all tests. So, I tried to implement the copy_in_string function in order to pass the open-bad-ptr test. THAT test did pass. However, whenever I called copy_in_string, ALL of the close tests were failing. So, I changed open to not call copy_in_string again. I did add a check to see if the file is null in open_file, and that passed open-null. 30 tests down, 50 to go!

--- pintos/src/userprog/open.sh
+++ pintos/src/userprog/open.sh
@@ -15,6 +15,17 @@ echo Entering directory /userprog/build ...
 cd build
 pintos-mkdisk filesys.dsk --filesys-size=2;
 
-# pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-normal -a open-normal -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-normal
+pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-normal -a open-normal -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-normal
+read -rp 'OPEN-NORMAL done. Press enter to continue...' secondyn </dev/tty
+
+pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-missing -a open-missing -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-missing
+read -rp 'OPEN-MISSING done. Press enter to continue...' secondyn </dev/tty
+
+pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-boundary -a open-boundary -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-boundary
+read -rp 'OPEN-boundary done. Press enter to continue...' secondyn </dev/tty
+
 pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-null -a open-null -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-null
-# pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-normal -a open-normal -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-normal
+read -rp 'OPEN-NULL done. Press enter to continue...' secondyn </dev/tty
+
+pintos -v -k -T 60 --qemu  --filesys-size=2 -p tests/userprog/open-bad-ptr -a open-bad-ptr -p ../../tests/userprog/sample.txt -a sample.txt -- -q  -f run open-bad-ptr
+read -rp 'OPEN-BAD-PTR done. Press enter to continue...' secondyn </dev/tty
