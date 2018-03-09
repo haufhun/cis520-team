95e5f0ac935512ec47381a189f92ca995d10232f
Tyler <tjstock@ksu.edu>
2018-03-08 19:49:06 -0600

    Changed exception.c file to call our exit syscall instead of thread_exit().  Had to change our sys_exit_handle to non static function. Finished create, remove,  exec, filesize, open, read, write. close, handlers. I also created a functin to search through our lists to reduce duplicate code.

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -13,7 +13,7 @@ typedef int pid_t;
 static void syscall_handler (struct intr_frame *);
 
 static void sys_hault_handle(void);
-static void sys_exit_handle(int);
+void sys_exit_handle(int);
 static pid_t sys_exec_handle(const char *);
 static int sys_wait_handle(pid_t);
 static bool sys_create_handle(const char *, unsigned);
@@ -28,25 +28,23 @@ static void sys_close_handle(int);
 
 static void copy_in(int *, uint32_t *, size_t);
 static char * copy_in_string(char *);
-static void* check_addr(const void *);
+static void * check_addr(const void *);
+static struct file_descriptor * search_list(int, bool);
 
 static struct lock fs_lock;
 
 
-/* This Table and struct are taken from Project2Session.pdf
-    Their implementations are done by ourselves. */
-
 /* Prototype for a syscall function. */
 typedef int syscall_function (int, int, int);
 
-/* A system call. */
+/* A system call structure.*/
 struct syscall
 {
   size_t arg_cnt; /* Number of arguments. */
   syscall_function *func; /* Implementation. */
 };
 
-/* A file descriptor */
+/* A file descriptor structure. */
 struct file_descriptor 
 {
   int count;
@@ -56,7 +54,7 @@ struct file_descriptor
 
 
 
-/* Table of system calls. */
+/* Table of system calls. This struct is taken from Project2Session.pdf. */
 static const struct syscall syscall_table[] =
 {
   {0, (syscall_function *) sys_hault_handle},      // halt                                               
@@ -80,9 +78,8 @@ void syscall_init (void)
   lock_init(&fs_lock);
 }
 
-/* This method is taken from the Proj2Session.pdf 
-    document. The handle methods are ipmlemented 
-    on our own accord. */
+/* This method is taken from the Proj2Session.pdf document. 
+    The handle methods are ipmlemented on our own accord. */
 static void syscall_handler (struct intr_frame *f)
 {
   const struct syscall *sc;
@@ -102,8 +99,7 @@ static void syscall_handler (struct intr_frame *f)
   memset (args, 0, sizeof (args));
   copy_in (args, (uint32_t *) f->esp + 1, sizeof(*args) * sc->arg_cnt);
   
-  /* Execute the system call,
-  and set the return value. */
+  /* Execute the system call, and set the return value. */
   f->eax = sc->func (args[0], args[1], args[2]);
 }
 
@@ -112,7 +108,7 @@ static void sys_hault_handle(void)
   shutdown_power_off();
 }
 
-static void sys_exit_handle(int status)
+void sys_exit_handle(int status)
 {
   struct thread *t = thread_current();
 
@@ -127,14 +123,62 @@ static pid_t sys_exec_handle(const char *file)
 {
   check_addr(file);
   
-  ASSERT (false);
+
+  lock_acquire(&fs_lock);
+	char * fn_cp = malloc (strlen(file)+1);
+  strlcpy(fn_cp, file, strlen(file)+1);
+  
+  char * save_ptr;
+  fn_cp = strtok_r(fn_cp," ",&save_ptr);
+
+  struct file* f = filesys_open (fn_cp);
+
+  if(f==NULL)
+  {
+    lock_release(&fs_lock);
+    return -1;
+  }
+  else
+  {
+    file_close(f);
+    lock_release(&fs_lock);
+    return process_execute(file);
+  }
 }
 
 static int sys_wait_handle(pid_t pid)
 {
   check_addr(&pid);
+
+  // struct list_elem *e;
+
+  // struct child *ch=NULL;
+  // struct list_elem *e1=NULL;
+
+  // for (e = list_begin (&thread_current()->child_proc); e != list_end (&thread_current()->child_proc);
+  //          e = list_next (e))
+  //       {
+  //         struct child *f = list_entry (e, struct child, elem);
+  //         if(f->tid == pid)
+  //         {
+  //           ch = f;
+  //           e1 = e;
+  //         }
+  //       }
+
+
+  // if(!ch || !e1)
+  //   return -1;
+
+  // thread_current()->waitingon = ch->tid;
+    
+  // if(!ch->used)
+  //   sema_down(&thread_current()->child_lock);
+
+  // int temp = ch->exit_error;
+  // list_remove(e1);
   
-  ASSERT(false);
+  // return temp;
 }
 
 
@@ -153,9 +197,15 @@ static bool sys_create_handle(const char *file, unsigned initial_size)
 
 static bool sys_remove_handle(const char *file)
 {
+  bool success;
+
   check_addr(file);
+
+  lock_acquire(&fs_lock);
+  success = filesys_remove(file);
+  lock_release(&fs_lock);
   
-  ASSERT (false);
+  return success;
 }
 
 static int sys_open_handle (const char *file)
@@ -193,174 +243,151 @@ static int sys_open_handle (const char *file)
 
 static int sys_filesize_handle(int fd_num)
 {
-  struct list_elem *e;
-	struct file_descriptor *fd;
-  struct thread *t = thread_current ();
-  bool didIt = false;
+  struct file_descriptor *fd;  
   int length;
-  
-  // if(fd_num <= STDOUT_FILENO)
-  //   return -1;
+
+  if(fd_num <= STDOUT_FILENO)
+  return;
+
 
   lock_acquire(&fs_lock);
-
-  for (e = list_begin (&t->fd_list); e != list_end (&t->fd_list); e = list_next (e))
+  fd = search_list(fd_num, false);
+  if(fd)
   {
-    fd = list_entry (e, struct file_descriptor, elem);
-
-    if(fd->count == fd_num)
-    {
-      length = file_length(fd->fp);
-      didIt = true;
-    }
-  }
-
-  if(didIt)
+    length = file_length(fd->fp);
     free(fd);
-
-  lock_release(&fs_lock);    
+  }
+  lock_release(&fs_lock);
+  
   return length;
 }
 
-static int sys_read_handle(int fd, char * buffer, unsigned size)
+static int sys_read_handle(int fd_num, char * buffer, unsigned size)
 {
-  int i;
+  struct file_descriptor *fd;  
+  int bytes_read = -1;
 
-  check_addr(buffer);  
-  check_addr(&fd);  //Not sure if this is the right way to do it, but it passes the read-bad-fd test.
+  check_addr(buffer);    
   
-  
-  if(fd < STDIN_FILENO)
+  if(fd_num < STDIN_FILENO)
     return -1;
 
-  if(fd == STDIN_FILENO)
+  if(fd_num == STDIN_FILENO)
   {
+    int i;
     for (i = 0; i < size; i++)
       buffer[i] = input_getc();
 
     return size;
   }
   
-  if(fd > STDOUT_FILENO)
+  if(fd_num > STDOUT_FILENO)
   {
-    ASSERT (false);
-    // need to implement writing to a file here
+    lock_acquire(&fs_lock);
+    fd = search_list(fd_num, false);
+    if(fd)
+    {
+      bytes_read = file_read(fd->fp, buffer, size);
+      free(fd);
+    }
+    lock_release(&fs_lock);
   }
+  return bytes_read;
 }
 
-static int sys_write_handle(int fd, char * buffer, unsigned size)
+static int sys_write_handle(int fd_num, char * buffer, unsigned size)
 {
-  int i;
+  struct file_descriptor *fd;   
+  int bytes_read = -1;
   
   check_addr(buffer);
-  //check_addr(&fd);  //this crashes all the tests for some reason... Worked for read-bad-fd though.
   
-  if(fd < STDOUT_FILENO)
+  if(fd_num < STDOUT_FILENO)
     return -1;
 
-  if(fd == STDOUT_FILENO)
+  if(fd_num == STDOUT_FILENO)
   {
+    int i;
     for (i = 0; i < size; i++)
       printf("%c", buffer[i]);
 
     return size;
   }
   
-  if(fd > STDOUT_FILENO)
+  if(fd_num > STDOUT_FILENO)
   {
-    ASSERT (false);
-    // need to implement writing to a file here
+    lock_acquire(&fs_lock);
+    fd = search_list(fd_num, false);
+    if(fd)
+    {
+      bytes_read = file_write(fd->fp, buffer, size);
+      free(fd);
+    }
+    lock_release(&fs_lock);
   }
+  return bytes_read;  
 }
-
 
 static void sys_seek_handle(int fd_num, unsigned position)
 {
-  struct list_elem *e;
-	struct file_descriptor *fd;
-  struct thread *t = thread_current ();
-  bool didIt = false;
+  struct file_descriptor *fd;
   
   if(fd_num <= STDOUT_FILENO)
     return;
 
-  lock_acquire(&fs_lock);
-  for (e = list_begin (&t->fd_list); e != list_end (&t->fd_list); e = list_next (e))
-  {
-    fd = list_entry (e, struct file_descriptor, elem);
 
-    if(fd->count == fd_num)
-    {
-      file_seek(fd->fp, position);
-      didIt = true;
-    }
+  lock_acquire(&fs_lock);
+  fd = search_list(fd_num, false);
+  if(fd)
+  {
+    file_seek(fd->fp, position);
+    free(fd);
   }
 
-  if(didIt)
-    free(fd);
-
-  lock_release(&fs_lock);    
+  lock_release(&fs_lock);
+  
+  return;
 }
 
 static unsigned sys_tell_handle(int fd_num)
 {
-  struct list_elem *e;
-	struct file_descriptor *fd;
-  struct thread *t = thread_current ();
-  bool didIt = false;
+  struct file_descriptor *fd;  
   unsigned file_pos;
-  
+
   if(fd_num <= STDOUT_FILENO)
     return;
 
+
   lock_acquire(&fs_lock);
-  for (e = list_begin (&t->fd_list); e != list_end (&t->fd_list); e = list_next (e))
+  fd = search_list(fd_num, false);
+  if(fd)
   {
-    fd = list_entry (e, struct file_descriptor, elem);
-
-    if(fd->count == fd_num)
-    {
-      file_pos = file_tell(fd->fp);
-      didIt = true;
-    }
-  }
-
-  if(didIt)
+    file_pos = file_tell(fd->fp);
     free(fd);
-
-  lock_release(&fs_lock); 
+  }
+  lock_release(&fs_lock);
   
   return file_pos;
 }
 
 static void sys_close_handle(int fd_num)
 {
-	struct list_elem *e;
-	struct file_descriptor *fd;
-  struct thread *t = thread_current ();
-  bool didIt = false;
+  struct file_descriptor *fd;
 
   if(fd_num <= STDOUT_FILENO)
     return;
 
+
   lock_acquire(&fs_lock);
-  
-  for (e = list_begin (&t->fd_list); e != list_end (&t->fd_list); e = list_next (e))
+  fd = search_list(fd_num, true);
+  if(fd)
   {
-    fd = list_entry (e, struct file_descriptor, elem);
-
-    if(fd->count == fd_num)
-    {
-      file_close(fd->fp);
-      list_remove(e);
-      didIt = true;
-    }
-  }
-
-  if(didIt)
+    file_close(fd->fp);
     free(fd);
-
-  lock_release(&fs_lock);    
+  }
+  lock_release(&fs_lock);
+  
+  return;
 }
 
 static void copy_in(int * argv, uint32_t *stp, size_t size)
@@ -397,7 +424,7 @@ static char * copy_in_string(char * string)
 }
 
 /* This method checks if the parementer addr is a bad address/pointer */
-void* check_addr(const void *addr)
+static void* check_addr(const void *addr)
 {
   void *page_ptr = NULL;
 
@@ -415,4 +442,26 @@ void* check_addr(const void *addr)
 	}
 
 	return page_ptr;
+}
+
+/* Searches through the lsit for the correspending file_descriptor then returns it */
+static struct file_descriptor* search_list(int fd_num, bool remove_list_elem)
+{
+  struct list_elem *e;
+	struct file_descriptor *fd;
+  struct thread *t = thread_current ();
+
+  for (e = list_begin (&t->fd_list); e != list_end (&t->fd_list); e = list_next (e))
+  {
+    fd = list_entry (e, struct file_descriptor, elem);
+
+    if(fd->count == fd_num)
+    {
+      if(remove_list_elem) 
+        list_remove(e);
+
+      return fd;
+    }
+  }
+  return NULL;
 }
