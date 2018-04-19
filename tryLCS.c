81d6898dd96c0c9dd8d744458755e21bae3ffad7
Tyler <tjstock@ksu.edu>
2018-04-19 13:02:20 -0500

    added tryLCS.c file

--- tryLCS.c
+++ tryLCS.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void getLCS( char *X, char *Y, int lengthX, int lengthY );
+#include <limits.h>
+void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineNum2 );
 void clearArray(char *array, int lengthArray);
-
+#define MAXCHAR 1000
 int main(void)
 {
-	char X[] = "AGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOLLOOTABbb";
-  	char Y[] = "GXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOGGHEOLLOOTABbbAGGHEOLLOOTABbbAGGHEOBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbbGXTXHEOLLOOAYBbb";
-  	int lengthX = strlen(X);
- 	int lengthY = strlen(Y);
-  	getLCS(X, Y, lengthX, lengthY);
+	char* filename = "../wiki_dump.txt";
+	char X[MAXCHAR];
+	char Y[MAXCHAR];
+	int lengthX, lengthY, count = 0;
+	FILE *fp;
+    
+ 
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+
+
+
+	fgets(X, MAXCHAR, fp);
+	
+    while (fgets(Y, MAXCHAR, fp) != NULL && count <10000)
+	{		
+		lengthX = strlen(X);
+	 	lengthY = strlen(Y);
+		
+	 	getLCS(X, Y, lengthX, lengthY, count, count+1);
+		// printf("\n%s\n", X);
+		strcpy(X,Y);
+		// printf("\n%s\n", X);
+		count++;
+	}
+	fclose(fp);
   	return 0;
 }
 
@@ -19,7 +44,7 @@ int main(void)
 	EX. String1 = "333ABCD455"
 		String2 = "55652ABCD44456"
 		LCS = "ABCD" 							*/
-void getLCS( char *X, char *Y, int lengthX, int lengthY )
+void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineNum2 )
 {
 	/* Common string cant be longer than the shortest string, 
 		therefore it does not matter which length you use to 
@@ -54,7 +79,7 @@ void getLCS( char *X, char *Y, int lengthX, int lengthY )
 			}
 		}	
 	}
-    printf("%s\n", LCS);
+    printf("%d-%d: %s\n",lineNum1,lineNum2, LCS);
 }
 
 /* Clears the given array */
