626181d428ef2cd45ab160ef918ba4668f1f1055
Tyler Stock <tjstock@selene.beocat.ksu.edu>
2018-04-21 17:46:20 -0500

    attemp to read in sections

--- LCS.c
+++ LCS.c
@@ -5,22 +5,23 @@
 #include <limits.h>
 void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineNum2 );
 void clearArray(char *array, int lengthArray);
-void ReadFileIntoArray(void);
+void ReadFileIntoArray(int startPos, int endPos, char** local_array);
 void DoLCS(int);
 void PrintArray(void);
+void CountLines();
 
 #define NUM_THREADS 4
 #define ARRAY_SIZE 1000000
-char char_array[ARRAY_SIZE][2005];
+//char char_array[ARRAY_SIZE][2005];
 char char_array2[ARRAY_SIZE][2005];
-
-unsigned int count = 0;
+unsigned int line_count = 0;
 
 int main(void)
 {
     omp_set_num_threads(NUM_THREADS);
 
-	ReadFileIntoArray();
+	//ReadFileIntoArray();
+    CountLines();
     #pragma omp parallel 
 	{
         DoLCS(omp_get_thread_num());
@@ -33,22 +34,25 @@ int main(void)
 void PrintArray()
 {
     int i;
-    for(i = 0; i < count; i ++)
+    for(i = 0; i < line_count; i ++)
     {
-         printf("%d-%d: %s\n",i,i+1, char_array2[i]);
+         printf("%d-%d: %s\n", i, i+1, char_array2[i]);
     }
 }
 
 void DoLCS(int myID)
 {
-    int lengthX, lengthY, i, startPos, endPos;
+    int lengthX, lengthY, i,j, startPos, endPos;
     
-    #pragma omp private(myID,theChar,charLoc,local_char_count,startPos,endPos,i,j)
+    #pragma omp private(myID, i, j, lengthX, lengthY, startPos, endPos)
     {
-        startPos = myID * (count / NUM_THREADS);
-		endPos = startPos + (count / NUM_THREADS);
+        startPos = myID * (line_count / NUM_THREADS);
+		endPos = startPos + (line_count / NUM_THREADS);
         
-        for(i = startPos; i < endPos; i++)
+        char char_array[endPos-startPos][2005];
+        ReadFileIntoArray(startPos, endPos, (char**)char_array);
+        
+        for(i = startPos, j = 0; i < endPos; i++, j++)
         {
            lengthX = strlen(char_array[i]);
            lengthY = strlen(char_array[i+1]);
@@ -67,7 +71,7 @@ void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineN
 		initilize the array. 							*/
 	char LCS[lengthX];
 	char tempLCS[lengthX];
-	int i, j, t, tempJ, count = 0;
+	int i, j, t, tempJ, line_count = 0;
 
 	for(i = 0; i < lengthX; i ++)
 	{
@@ -78,10 +82,10 @@ void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineN
 				clearArray(tempLCS, lengthX);
 				
 				tempJ = j;
-				count = 0;				
+				line_count = 0;				
 				while(X[i] == Y[tempJ] && i < lengthX && tempJ < lengthY)
 				{
-                  	tempLCS[count++] =  X[i++];
+                  	tempLCS[line_count++] =  X[i++];
 				    tempJ++;
 				}
 
@@ -102,26 +106,47 @@ void getLCS( char *X, char *Y, int lengthX, int lengthY, int lineNum1, int lineN
     strcpy(char_array2[lineNum1], LCS);
 }
 
-void ReadFileIntoArray()
+void ReadFileIntoArray(int startPos, int endPos, char** local_array)
 {
     char* filename = "../wiki_dump.txt";
-	
-	
 	FILE *fp;
-    
- 
+    int local_count=0;
+    int another_count = startPos;
     fp = fopen(filename, "r");
     if (fp == NULL){
         printf("Could not open file %s",filename);
         return;
     }
-	
-    while (fgets(char_array[count], ARRAY_SIZE, fp) != NULL)
+    char line[2005];
+    while (fgets(line, ARRAY_SIZE, fp) != NULL)
 	{		
-		 count++;
+        if(another_count <=endPos)
+        {    
+            strcpy(local_array[local_count],line);
+            another_count++;
+        }
+		local_count++;      
+	}    
+	fclose(fp);
+    return;
+}
+void CountLines()
+{
+    char* filename = "../wiki_dump.txt";
+	FILE *fp;
+    
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return;
+    }
+    char line[2005];
+    while (fgets(line, ARRAY_SIZE, fp) != NULL)
+	{		
+		 line_count++;
          
 	}    
-    printf("count: %d\n\n\n\n", count);
+    printf("line_count: %d\n\n\n", line_count);
 	fclose(fp);
 }
 /* Clears the given array */
